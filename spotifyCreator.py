736627757f1a3bdda93f953e2483f464b8f61b67
kevin-gottlieb <kgred9@gmail.com>
2019-08-05 11:37:01 -0700

    Get Tracks from JSON in spotifyCreator

--- spotifyCreator.py
+++ spotifyCreator.py
@@ -1,6 +1,20 @@
-import spotipy
 import json
+import spotipy as sp
+from spotipy.oauth2 import SpotifyClientCredentials
 
 with open('autoCrawler.json', 'r') as crawlerFile:
 	data = json.load(crawlerFile)
-	print(data)
+	spotify = sp.Spotify()
+	songTestLink = 'spotify:track:3MECtlvsjOopA66odGHa5P'
+	client_credentials_manager = SpotifyClientCredentials()
+	sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+	playlists = sp.user_playlists('spotify')
+	for track in data:
+		# Get data for each track
+		albumName = track['albumName']
+		trackTitle = track['Title']
+		trackArtist = track['Artist']
+		# Spotify search
+		# results = sp.search(q=)
+	
