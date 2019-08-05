ddd36e8a9307c5044729fd3d1740d428fc96a342
kevin-gottlieb <kgred9@gmail.com>
2019-08-05 16:49:56 -0700

    Successful Spotify Auth and Read

--- spotifyCreator.py
+++ spotifyCreator.py
@@ -1,20 +1,40 @@
+import sys
 import json
 import spotipy as sp
 from spotipy.oauth2 import SpotifyClientCredentials
+import spotipy.util as util
+
+CLIENT_ID='a66d208e17874246af021cc9b819400d'
+CLIENT_SECRET='e79e635054f9404792560a1d99eacc7b' # will be reset after use
+REDIRECT_URI='http://localhost:8888/'
+scope = 'user-library-read playlist-modify-public'
+songTestLink = 'spotify:track:3MECtlvsjOopA66odGHa5P'
+
+#get username from console
+if len(sys.argv) > 1:
+    username = sys.argv[1]
+    print('username: ' + username)
+else:
+    print('Need username')
+    sys.exit()
 
 with open('autoCrawler.json', 'r') as crawlerFile:
 	data = json.load(crawlerFile)
-	spotify = sp.Spotify()
-	songTestLink = 'spotify:track:3MECtlvsjOopA66odGHa5P'
-	client_credentials_manager = SpotifyClientCredentials()
-	sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+	token = util.prompt_for_user_token(username, scope, client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI)
+	
+	if token:
+		spotify = sp.Spotify(auth=token)
+		results = spotify.current_user_saved_tracks()
+		print(len(results))
+		for item in results['items']:
+			track = item['track']
+			print(track['name'] + ' - ' + track['artists'][0]['name'])
 
-	playlists = sp.user_playlists('spotify')
-	for track in data:
-		# Get data for each track
-		albumName = track['albumName']
-		trackTitle = track['Title']
-		trackArtist = track['Artist']
-		# Spotify search
-		# results = sp.search(q=)
+	# for track in data:
+		# # Get data for each track
+		# albumName = track['albumName']
+		# trackTitle = track['Title']
+		# trackArtist = track['Artist']
+		# # Spotify search
+		# # results = sp.search(q=)
 	
