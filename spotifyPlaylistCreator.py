8a42deb7dcee5513d68bfe29f84abab59c5e7e58
kevin-gottlieb <kgred9@gmail.com>
2019-08-05 21:39:54 -0700

    Create Playlists only if they do not already exist
    - Rename spotifyCreator.py to spotifyPlaylistCreator.py

--- spotifyPlaylistCreator.py
+++ spotifyPlaylistCreator.py
@@ -7,11 +7,11 @@ import spotipy.util as util
 CLIENT_ID='a66d208e17874246af021cc9b819400d'
 CLIENT_SECRET='e79e635054f9404792560a1d99eacc7b' # will be reset after use
 REDIRECT_URI='http://localhost:8888/'
-scope = 'user-library-read playlist-modify-public playlist-read-private'
-songTestLink = 'spotify:track:3MECtlvsjOopA66odGHa5P'
+scope = 'playlist-modify-public playlist-read-private'
 
 def createPlaylists(jsonData, spot, user):
 	r'''Create a playlist for every unique album.
+	Skips playlists if they already exist
 	
 	Arguments:
 	jsonData -- data extracted by timeLifeAutoCrawler.py including Album Name, Track Name, and Artist
@@ -20,8 +20,16 @@ def createPlaylists(jsonData, spot, user):
 	'''
 	
 	albumNames = set([track['albumName'] for track in data])
+	playlists = spotify.user_playlists(user)
+	# Check for playlists that already exists
+	playlistNames = []
+	for playlist in playlists['items']:
+		if playlist['owner']['id'] == user:
+			playlistNames.append(playlist['name'])
+				
 	for albumName in albumNames:
-		print(spot.user_playlist_create(user, albumName))
+		if not albumName in playlistNames:
+			spot.user_playlist_create(user, albumName)
 		
 
 #get username from console
